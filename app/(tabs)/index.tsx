08c30a9e1ba526f8b9f8be83971b81c23ca10bb4
lorena.silva <lorena.silva@g.globo>
2026-05-17 14:19:50 -0300

    teste 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import * as Location from "expo-location";
 import { useEffect, useRef, useState } from "react";
 import {
-  ActivityIndicator, //teste
+  ActivityIndicator,
   Animated,
   Easing,
   Linking,
